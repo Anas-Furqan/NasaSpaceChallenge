01bd39f9b505fd08d58ff124cd1b4dd5e02399db
Anas Furqan <codewithanas12@gmail.com>
2024-10-05 23:42:18 -0700

    ..

--- js/script.js
+++ js/script.js
@@ -2,30 +2,30 @@
 
 
 // carousel 
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop: true, // Ensures that the carousel loops correctly
-    margin: 10,
-    nav: true, // Enables navigation arrows
-    autoplay: true, // Automatically starts the carousel
-    autoplayTimeout: 2500, // 2.5 seconds delay for autoplay
-    autoplayHoverPause: true, // Pause on hover
-    navText: [
-      '<i class="fas fa-chevron-left"></i>', // Custom FontAwesome left arrow
-      '<i class="fas fa-chevron-right"></i>' // Custom FontAwesome right arrow
-    ],
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 1
-      },
-      1000: {
-        items: 1
-      }
-    }
-  });
-});
+// $(document).ready(function(){
+//   $(".owl-carousel").owlCarousel({
+//     loop: true, // Ensures that the carousel loops correctly
+//     margin: 10,
+//     nav: true, // Enables navigation arrows
+//     autoplay: true, // Automatically starts the carousel
+//     autoplayTimeout: 2500, // 2.5 seconds delay for autoplay
+//     autoplayHoverPause: true, // Pause on hover
+//     navText: [
+//       '<i class="fas fa-chevron-left"></i>', // Custom FontAwesome left arrow
+//       '<i class="fas fa-chevron-right"></i>' // Custom FontAwesome right arrow
+//     ],
+//     responsive: {
+//       0: {
+//         items: 1
+//       },
+//       600: {
+//         items: 1
+//       },
+//       1000: {
+//         items: 1
+//       }
+//     }
+//   });
+// });
 
 
